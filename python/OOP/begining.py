1556b87365ebe761c95e35e88e66866d694a72f7
Yohannes <john.shewakena@gmail.com>
2025-07-29 16:09:30 +0300

    oop

--- python/OOP/begining.py
+++ python/OOP/begining.py
@@ -4,13 +4,3 @@ class car:
 
     def wheel(self):
         print("The car has 4 wheels.")
-
-
-class electric_car(car):
-    def seats(self):
-        print(f"The electric car with the model {self.model} has 5 seats.")
-
-
-BYD = electric_car("BYD")
-BYD.wheel()
-BYD.seats()
